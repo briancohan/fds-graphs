7cf8c74309fda9542b95107da1c68ca630b5df09
briancohan <briancohan@gmail.com>
2020-10-07 16:15:23 -0400

    simple graph

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
 
 st.header("It Worked!")
+
+df = pd.DataFrame({
+    'x': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    'y': [1, 2, 3, 2, 1, 2, 3, 2, 1],
+})
+st.altair_chart(
+    alt.Chart(df).mark_line().encode(x='x', y='y'),
+    use_container_width=True
+)
